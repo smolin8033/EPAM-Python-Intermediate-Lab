c0120f2bcbb14cef488f117522576269ae9432fa
Александр Смолин <smolll123@mail.ru>
2022-02-09 14:35:44 +0300

    wrote test funcs for checking the correct results after passing zeros and step as params

--- hw2_testing/test_main.py
+++ hw2_testing/test_main.py
@@ -30,3 +30,11 @@ def test_my_range_give_2_strings():
 
 def test_my_range_pos_pos():
     assert my_range(1, 5) == [1, 2, 3, 4]
+
+
+def test_my_range_zero():
+    assert my_range(0, 0) == [0]
+
+
+def test_my_range_with_step():
+    assert my_range(1, 9, 4) == [1, 5]
