cd160a36ce71c70977728ddb0c90c1385403d4ba
Александр Смолин <smolll123@mail.ru>
2022-02-25 21:26:49 +0300

    writing args for parsing from command line

--- hw4_parallel_programming/main.py
+++ hw4_parallel_programming/main.py
@@ -1,1 +1,23 @@
+import argparse
+# import os
+
+
 errors_counter = 0
+
+parser = argparse.ArgumentParser(description='Give entry parameters')
+
+# python download.py urllist.txt --dir=thumbnails/ --threads=4 --size=128x128
+parser.add_argument('file')
+parser.add_argument('--dir', default='os.getcwd()')
+parser.add_argument('--threads', type=int, default=1)
+parser.add_argument('--size', type=str, default='100x100')
+
+
+args = parser.parse_args()
+
+
+print('Argument values:')
+print(args.file)
+print(args.dir)
+print(args.threads)
+print(args.size)
