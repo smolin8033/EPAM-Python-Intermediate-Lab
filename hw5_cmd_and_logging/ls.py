6d16ccf20a78c18dfb269d75fe5a803aca6e8b0e
Александр Смолин <smolll123@mail.ru>
2022-03-05 15:15:15 +0300

    writing ls_a and sort_and_output functions

--- hw5_cmd_and_logging/ls.py
+++ hw5_cmd_and_logging/ls.py
@@ -4,16 +4,29 @@ import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('dir', type=str, nargs='?', default='.')
-parser.add_argument('-a', action='store_true')
+parser.add_argument('-a', action='store_true', help='List all files, including all "." files')
 parser.add_argument('-l', action='store_true')
 
 args = parser.parse_args()
 
 
-def ls():
-    # if
-    for file in os.listdir(args.dir):
+def sort_and_output(files):
+    files.sort()
+    for file in files:
         print(file)
+
+
+def ls_a():
+    files = [file for file in os.listdir(args.dir)]
+    files += [os.curdir, os.pardir]
+    sort_and_output(files)
+
+
+def ls():
+    if args.a:
+        ls_a()
+    files = [file for file in os.listdir(args.dir) if file[0] != '.']
+    sort_and_output(files)
 
 
 if __name__ == '__main__':
