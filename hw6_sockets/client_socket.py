ac4dcd5fda57752f93f5e7c14f8790f2f85435f0
Александр Смолин <smolll123@mail.ru>
2022-03-12 16:33:05 +0300

    make some changes

--- hw6_sockets/client_socket.py
+++ hw6_sockets/client_socket.py
@@ -1,13 +1,17 @@
+import random
 import socket
+import time
+from datetime import datetime
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(('192.168.0.10', 9999))
 
 while True:
-    data = input()
-    if data == 'q':
-        break
+    time_now = datetime.now().strftime('%H:%M:%S')
+    data = f'Current time: {time_now}, value: {random.randint(1, 10000)}'
     sock.send(data.encode('utf-8'))
     response = sock.recv(1024)
     print(response.decode('utf-8'))
+    time.sleep(60)
+
 sock.close()
--- hw6_sockets/server_socket.py
+++ hw6_sockets/server_socket.py
@@ -7,6 +7,12 @@ sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind(('192.168.0.10', 9999))
 sock.listen(5)
 
+
+def save_to_file(un_data):
+    with open('text_file.txt', 'a') as file:
+        file.write('{}\n'.format(un_data))
+
+
 while True:
     client, addr = sock.accept()
     print('Connected: ', addr)
@@ -16,6 +22,7 @@ while True:
         if not data:
             break
         udata = data.decode('utf-8')
+        save_to_file(udata)
         client.send(udata.upper().encode('utf-8'))
 
     client.close()
