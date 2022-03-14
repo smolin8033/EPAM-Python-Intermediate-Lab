49367ed7a84a9f301765d95dc762463c80180b59
Александр Смолин <smolll123@mail.ru>
2022-03-14 16:35:17 +0300

    make some changes

--- hw6_sockets/client_socket.py
+++ hw6_sockets/client_socket.py
@@ -10,15 +10,16 @@ sock.connect(('192.168.0.10', 9999))
 w = wmi.WMI(namespace='root/OpenHardwareMonitor')
 
 
-while True:
-    temperature_info = ', '.join([f'{sensor.Name}: {sensor.Value}°C'
-                                  for sensor in w.Sensor() if sensor.SensorType == u'Temperature'])
-    time_now = datetime.now().strftime('%H:%M:%S')
-    data = f'Current time: {time_now}, random value: {random.randint(1, 10000)}, ' \
-           f'% CPU: {psutil.cpu_percent()}, {temperature_info}'
-    sock.send(data.encode('utf-8'))
-    response = sock.recv(1024)
-    print(response.decode('utf-8'))
-    time.sleep(60)
-
-sock.close()
+try:
+    while True:
+        temperature_info = ', '.join([f'{sensor.Name}: {sensor.Value}°C'
+                                      for sensor in w.Sensor() if sensor.SensorType == u'Temperature'])
+        time_now = datetime.now().strftime('%H:%M:%S')
+        data = f'Current time: {time_now}, random value: {random.randint(1, 10000)}, ' \
+               f'% CPU: {psutil.cpu_percent()}, {temperature_info}'
+        sock.send(data.encode('utf-8'))
+        response = sock.recv(1024)
+        print(response.decode('utf-8'))
+        time.sleep(60)
+except KeyboardInterrupt:
+    sock.close()
