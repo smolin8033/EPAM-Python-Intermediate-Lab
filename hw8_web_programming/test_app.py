2f30713258a39b6940a42a81204a82bead819029
Александр Смолин <smolll123@mail.ru>
2022-03-27 18:45:09 +0300

    make some changes

--- hw8_web_programming/test_app.py
+++ hw8_web_programming/test_app.py
@@ -8,7 +8,6 @@ response = requests.get('http://localhost:5000/api/recipes/1')
 
 print(response.status_code)
 
-
 sample = {
     # 'id': request.json['id'] or RECIPES[-1]['id'] + 1,
     'meal': 'Lasagna',
@@ -17,7 +16,14 @@ sample = {
     'directions': 'Cook them',
 }
 
-
 response = requests.post('http://localhost:5000/api/recipes/', json=sample)
 
 print(response.json())
+
+response = requests.put('http://localhost:5000/api/recipes/update/1', json=sample)
+
+print(response.json())
+
+response = requests.delete('http://localhost:5000/api/recipes/delete/2', json=sample)
+
+print(response)
