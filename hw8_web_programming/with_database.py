f01ca44beb3f0252c851b3402e290ce9dd04baec
Александр Смолин <smolll123@mail.ru>
2022-03-28 17:53:51 +0300

    make some changes

--- hw8_web_programming/with_database.py
+++ hw8_web_programming/with_database.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, abort, jsonify, request
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 
@@ -47,3 +47,45 @@ def read_recipes():
 def retrieve_recipe(recipe_id):
     recipe = Recipe.query.get_or_404(recipe_id)
     return recipe_schema.jsonify(recipe)
+
+
+@app.route('/api_v2/recipes/', methods=['POST'])
+def create_recipe():
+    try:
+        recipe = Recipe(
+            meal=request.json['meal'],
+            description=request.json['description'],
+            ingredients=request.json['ingredients'],
+            directions=request.json['directions']
+        )
+
+        db.session.add(recipe)
+        db.session.commit()
+        return recipe_schema.jsonify(recipe), 201
+    except KeyError:
+        abort(400)
+
+
+@app.route('/api_v2/recipes/update/<int:recipe_id>', methods=['PUT'])
+def update_recipe(recipe_id):
+    try:
+        recipe_to_update = Recipe.query.get_or_404(recipe_id)
+
+        recipe_to_update.meal = request.json['meal']
+        recipe_to_update.description = request.json['description']
+        recipe_to_update.ingredients = request.json['ingredients']
+        recipe_to_update.directions = request.json['directions']
+
+        db.session.commit()
+        return recipe_schema.jsonify(recipe_to_update)
+    except KeyError:
+        abort(400)
+
+
+@app.route('/api_v2/recipes/delete/<int:recipe_id>', methods=['DELETE'])
+def delete_recipe(recipe_id):
+    recipe_to_delete = Recipe.query.get_or_404(recipe_id)
+    db.session.delete(recipe_to_delete)
+
+    db.session.commit()
+    return recipe_schema.jsonify(recipe_to_delete), 204
